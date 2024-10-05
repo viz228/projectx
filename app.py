34caa2fb7958aaab47a297027bbee686e61318d8
Vijit Singh <sports.football99@gmail.com>
2024-10-05 14:54:36 +0530

    rajesh new work added

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ st.title('Hello world')
 st.text('this is an application project x')
 
 
-st.text('Rajesh work')
+st.text('this is my new rajesh work')
 
 
 st.text('trying to change in main branch directory')
 st.text('new work')
+
+st.text('rajes new work')
